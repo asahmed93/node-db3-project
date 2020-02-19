65410506cc9a9d6b236321391beff68db3ad787b
Lambda_School_Loaner_264 <lambda_school_loaner_264@Lambda-School-Loaner-264s-MacBook-Pro.local>
2020-02-19 17:22:14 -0500

    Added schemas

---
+++ schemes/scheme-model.js
@@ -0,0 +1,45 @@
+const db = require('../data/db-config');
+
+module.exports = {
+    find,
+    findById,
+    findSteps,
+    add,
+    update,
+    remove
+}
+
+function find(){
+    return db.select("*")
+    .from("schemes");
+}
+
+function findById(id){
+    return db("schemes")
+    .where({id})
+    .first();
+}
+
+function findSteps(id){
+    return db("steps")
+    .join('schemes', 'schemes.id', 'steps.scheme_id')
+    .select('schemes.id', 'schemes.scheme_name', 'steps.step_number', 'steps.instructions')
+    .where("scheme_id", id)
+}
+
+function add(scheme) {
+    return db("schemes")
+    .insert(scheme, "id");
+  }
+  
+  function update(id, changes) {
+    return db("schemes")
+      .where({ id })
+      .update(changes);
+  }
+  
+  function remove(id) {
+    return db("schemes")
+      .where({ id })
+      .del();
+  }
