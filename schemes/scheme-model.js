9906d6ee2e2bd513f459c03863ae31a4df9386ec
Lambda_School_Loaner_264 <lambda_school_loaner_264@Lambda-School-Loaner-264s-MacBook-Pro.local>
2020-02-19 17:31:42 -0500

    Added order to find steps

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -25,6 +25,7 @@ function findSteps(id){
     .join('schemes', 'schemes.id', 'steps.scheme_id')
     .select('schemes.id', 'schemes.scheme_name', 'steps.step_number', 'steps.instructions')
     .where("scheme_id", id)
+    .orderBy("steps.step_number");
 }
 
 function add(scheme) {
