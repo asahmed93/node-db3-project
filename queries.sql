6e04570fd3b6b87598e70f5ae081abb57235826c
Lambda_School_Loaner_264 <lambda_school_loaner_264@Lambda-School-Loaner-264s-MacBook-Pro.local>
2020-02-19 16:25:08 -0500

    Added Queries

--- queries.sql
+++ queries.sql
@@ -2,8 +2,35 @@
 
 -- Display the ProductName and CategoryName for all products in the database. Shows 77 records.
 
+SELECT productName, CategoryName FROM product JOIN category ON product.CategoryId = category.Id
+
 -- Display the order Id and shipper CompanyName for all orders placed before August 9 2012. Shows 429 records.
+
+Select [order].Id, shipper.companyName from [Order] Join Shipper 
+ON [order].ShipVia = shipper.id
+WHERE orderdate < '2012-08-09'
+
 
 -- Display the name and quantity of the products ordered in order with Id 10251. Sort by ProductName. Shows 3 records.
 
+SELECT p.productName, od.quantity
+From [order] as o
+JOIN orderdetail as od
+ON o.id = od.OrderID
+JOIN product as p
+ON od.productID = p.id
+WHERE o.id = 10251
+ORDER BY ProductName ASC
+
+
 -- Display the OrderID, Customer's Company Name and the employee's LastName for every order. All columns should be labeled clearly. Displays 16,789 records.
+
+
+SELECT o.id,
+       c.CompanyName,
+       e.lastname
+  FROM [order] AS o
+       JOIN
+       customer AS c ON o.customerID = c.id
+       JOIN
+       employee AS e ON o.employeeid = e.id;
